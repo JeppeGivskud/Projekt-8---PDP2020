f3de5bac993d9747efbf5170847dbdd78feb2482
Christoffer <clyhol20@student.aau.dk>
2024-02-23 10:33:11 +0100

    Added room structure

--- Socket Server/index.js
+++ Socket Server/index.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 //const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
-const http = require('http');
-const cors = require('cors');
-
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+const http = require("http");
+const cors = require("cors");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -13,33 +12,34 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-        connectionStateRecovery: {},
-        cors: {origin: "http://localhost:8081"}
+  connectionStateRecovery: {},
+  cors: { origin: "http://localhost:8081" },
 });
 
-
-
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`${socket.id} connected`);
-  socket.broadcast.emit('newClient', {
-    description: `${socket.id} connected`
-  })
-
-  socket.on('disconnect', () => {
-    socket.disconnect();
-    console.log(`${socket.io} user disconnected`);
+  socket.join("room1");
+  socket.broadcast.to("room1").emit("newClient", {
+    description: `${socket.id} has connected`,
   });
 
-  socket.on('message sent', (msg) => {
-    io.emit('message sent',`${socket.id} said: ${msg}`)
-    console.log('Message: ' + msg)
+  socket.on("disconnect", () => {
+    console.log(`${socket.io} user disconnected`);
+    io.emit.to("room1").emit("disconnection", {
+      description: `${socket.id} has disconnected`,
+    });
+  });
+
+  socket.on("message sent", (msg) => {
+    io.to("room1").emit("message sent", `${socket.id} said: ${msg}`);
+    console.log("Message: " + msg);
   });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
