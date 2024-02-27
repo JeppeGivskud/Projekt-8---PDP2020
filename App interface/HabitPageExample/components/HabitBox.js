30d830953da36f232bcb14367b588dfee8b1e797
JM <jakob2.mortensen@gmail.com>
2024-02-27 10:11:24 +0100

    nodemodules fuckery

--- App interface/HabitPageExample/components/HabitBox.js
+++ App interface/HabitPageExample/components/HabitBox.js
@@ -1,6 +1,22 @@
-import { Text } from "react-native";
+import { Text, StyleSheet, View, Pressable } from "react-native";
 
 export default function HabitBox({ color, name, count }) {
+    return (
+        <View style={[StyleSheet.container, { backgroundColor: color }]}>
+            <Pressable>
 
+            </Pressable>
 
+        </View>
+
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: "40%",
+        height: 150,
+        borderRadius: 20,
+    }
+
+});
