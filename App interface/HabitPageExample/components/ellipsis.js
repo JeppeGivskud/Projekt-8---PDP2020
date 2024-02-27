e15a16b6380508feaaa3115cfe8be76cdae77410
JM <jakob2.mortensen@gmail.com>
2024-02-27 10:37:41 +0100

    nodemodules fuckery

---
+++ App interface/HabitPageExample/components/ellipsis.js
@@ -0,0 +1,20 @@
+import { Text, StyleSheet, View, Pressable } from "react-native";
+import { SFSymbol } from "react-native-sfsymbols";
+
+export default function Ellipsis({ }) {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        color: "#fff",
+        height: 20,
+        width: 20,
+
+    }
+});
