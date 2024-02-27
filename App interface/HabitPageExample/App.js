210f7d816b0d2ce2685c30613ad2b247e926ce83
JeppeGivskud <jgivsk20@student.aau.dk>
2024-02-27 10:10:23 +0100

    Basic layout

--- App interface/HabitPageExample/App.js
+++ App interface/HabitPageExample/App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.column}>
+        <Button style={styles.box} title={"1"}></Button>
+        <Button style={styles.box} title={"2"}></Button>
+        <Button style={styles.box} title={"3"}></Button>
+        <Button style={styles.box} title={"4"}></Button>
+      </View>
+      <View style={styles.column}>
+        <Button style={styles.box} title={"5"}></Button>
+        <Button style={styles.box} title={"6"}></Button>
+        <Button style={styles.box} title={"7"}></Button>
+        <Button style={styles.box} title={"8"}></Button>
+      </View>
     </View>
   );
 }
@@ -13,8 +23,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  column: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "tomato",
+  },
+  box: {
+    color: "white",
+    width: "100%",
+    height: "100%",
   },
 });
