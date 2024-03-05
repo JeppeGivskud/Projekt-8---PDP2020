5d4d919b892360ce3a06e55ecbbb9eaad0a0906a
JM <jakob2.mortensen@gmail.com>
2024-03-05 19:24:19 +0100

    Muligvis kan man markere en habit som done på en ikke smart måde

--- App interface/HabitPageExample/components/navigationBar.js
+++
@@ -1,2 +0,0 @@
-//This document should create a way to select a certain page. It should always be visible on the current page
-//This bar could envolve widgets for tracking maybe mood quickly. Tapping the bar launches the mood widget and tapping it again opens a mood viewer page?
