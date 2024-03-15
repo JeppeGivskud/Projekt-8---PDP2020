6b818d80b133b736b77feaf817dea78f613081f7
JeppeGivskud <jgivsk20@student.aau.dk>
2024-03-15 10:19:16 +0100

    Data in  json esque format

--- App interface/HabitPageExample/components/HabitPage.js
+++ App interface/HabitPageExample/components/HabitPage.js
@@ -7,23 +7,48 @@ import {
     SafeAreaView,
     StatusBar,
     Dimensions,
+    FlatList,
 } from "react-native";
-import TestButtons from "./testComponents/TestButtons";
 import HabitBox from "./HabitBox";
-import HabitBoxes from "./testComponents/HabitBoxes";
+
 // TODO: The scrollable should keep scrolling for a little longer
 function HabitPage() {
+    const inputdata = [
+        { color: "tomato", name: "Ostesovs", count: 1 },
+        { color: "blue", name: "Ostesovs", count: 2 },
+        { color: "navy", name: "Ostesovs", count: 3 },
+        { color: "green", name: "Ostesovs", count: 4 },
+        { color: "red", name: "Ostesovs", count: 5 },
+        { color: "rgb(200,0,200)", name: "Ostesovs", count: 6 },
+        { color: "rgb(0,200,200)", name: "Ostesovs", count: 7 },
+        { color: "rgb(200,200,0)", name: "Ostesovs", count: 8 },
+        { color: "rgb(100,0,100)", name: "Ostesovs", count: 9 },
+        { color: "rgb(0,200,100)", name: "Ostesovs", count: 10 },
+        { color: "rgb(200,0,100)", name: "Ostesovs", count: 11 },
+        { color: "rgb(100,0,200)", name: "Ostesovs", count: 12 },
+    ];
+
     return (
         <View style={{ flex: 1 }}>
             <StatusBar />
-            <ScrollView
+            <FlatList
+                data={inputdata}
+                renderItem={({ item }) => (
+                    <HabitBox
+                        color={item.color}
+                        name={item.name}
+                        count={item.count}
+                    />
+                )}
+            />
+            {/* <ScrollView
                 style={{ flex: 1 }}
                 contentContainerStyle={styles.scrollview}
             >
                 <View style={styles.container}>
                     <HabitBoxes style={styles.container} />
                 </View>
-            </ScrollView>
+            </ScrollView> */}
         </View>
     );
 }
