0dd426d830a71764e082783c9d321e1e38b2f3af
JeppeGivskud <jgivsk20@student.aau.dk>
2024-05-10 14:14:30 +0200

    maybe it works

--- Server/countSocketPython/countsocket.py
+++ Server/countSocketPython/countsocket.py
@@ -1,22 +1,6 @@
-import eventlet
-import socketio # pip install python-socketio as well ¯\_(ツ)_/¯  
-
-sio = socketio.Server(cors_allowed_origins='http://localhost:8081')
-app = socketio.WSGIApp(sio, static_files={
-    '/': {'content_type': 'text/html', 'filename': 'index.html'}
-})
-
-@sio.event
-def connect(sid, environ):
-    print('connect ', sid)
-
-@sio.event
-def my_message(sid, data):
-    print('message ', data)
-
-@sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
+import Pi_components as Pi
+import countsocketsandbox as count
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 3000)), app)
+    Pi.main()
+    count.main()
---
+++ Server/countSocketPython/countsocketsandbox.py
@@ -0,0 +1,36 @@
+import eventlet
+import socketio
+import random
+
+socket = socketio.Server(async_mode='eventlet',cors_allowed_origins=["http://localhost:8081","http://hvejsel.dk:8080"])
+app = socketio.WSGIApp(socket)
+## ssh pi@G883
+## kode: 12345
+## ssh file transfer scp
+
+@socket.on('echo')
+def echo(sid, message):
+    socket.emit('echo', message)
+
+def worker1():
+    eventlet.wsgi.server(eventlet.listen(('', 3000)), app)
+
+def worker2():
+    while(1):
+        print("send clock")
+        socket.emit('newTodayValue', 40)  # Emit the event when a client connects
+        socket.sleep(1)
+        socket.emit('newTodayValue', 60)  # Emit the event when a client connects
+        socket.sleep(1)
+        socket.emit('newTodayValue', 60)  # Emit the event when a client connects
+        socket.sleep(1)
+        socket.emit('pressed', "pressoboie")  # Emit the event when a client connects
+        socket.sleep(1)
+
+
+def main():
+    socket.start_background_task(worker2)
+    worker1()
+
+if __name__ == '__main__':
+    main()
