8768bb2d6bf254c1623483e9a16384ef201d7e29
JeppeGivskud <jeppefirring@gmail.com>
2024-05-14 15:06:52 +0200

    shid

--- App interface/SQLReact/App.js
+++ App interface/SQLReact/App.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 // TODO: Flyt de her funktioner ud af App.js!
 // TODO: Variabel for IP-adresse
 
-const IP = `http://localhost:3000`
+const IP = `http://localhost:3000`;
 
 export default function App() {
     const [allData, setAllData] = useState();
@@ -20,48 +19,50 @@ export default function App() {
     // Create table if not exists
     const createTable = () => {
         fetch(`${IP}/createTable`)
-            .then(response => response.text())
-            .catch(error => console.error('Error:', error));
-    }
+            .then((response) => response.text())
+            .catch((error) => console.error("Error:", error));
+    };
 
     // Get all data
     const getAllData = () => {
         fetch(`${IP}/getData`)
-            .then(response => response.json())
-            .then(data => setAllData(data))
-            .catch(error => console.error('Error:', error));
+            .then((response) => response.json())
+            .then((data) => setAllData(data))
+            .catch((error) => console.error("Error:", error));
     };
 
     // Post count
     const postCount = (habitName) => {
         fetch(`${IP}/setCount?habitName=${encodeURIComponent(habitName)}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({ count: count }),
         })
-            .then(response => response.json())
-            .catch(error => console.error('Error:', error));
-    }
+            .then((response) => response.json())
+            .catch((error) => console.error("Error:", error));
+    };
 
     // Post effort
     const postEffort = (habitName) => {
         fetch(`${IP}/setEffort?habitName=${encodeURIComponent(habitName)}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({ effort: effort }),
         })
-            .then(response => response.json())
-            .then(data => setEffort(data))
-            .catch(error => console.error('Error:', error));
-    }
+            .then((response) => response.json())
+            .then((data) => setEffort(data))
+            .catch((error) => console.error("Error:", error));
+    };
 
     // Get target from latest habit row with same habitName. Bruges i newHabitRow til at få target med i den nye række
     async function getTarget(habitName) {
-        const response = await fetch(`${IP}/getTarget?habitName=${encodeURIComponent(habitName)}`)
+        const response = await fetch(
+            `${IP}/getTarget?habitName=${encodeURIComponent(habitName)}`
+        );
         const data = await response.json();
         setTarget(data);
         console.log(data);
@@ -70,7 +71,9 @@ export default function App() {
 
     // Get routine from latest habit row with same habitName. Bruges i newHabitRow til at få rutine med i den nye række
     async function getRoutine(habitName) {
-        const response = await fetch(`${IP}/getRoutine?habitName=${encodeURIComponent(habitName)}`)
+        const response = await fetch(
+            `${IP}/getRoutine?habitName=${encodeURIComponent(habitName)}`
+        );
         const data = await response.json();
         setRoutine(data);
         console.log(data);
@@ -82,22 +85,20 @@ export default function App() {
         const target = await getTarget(habitName);
         const routine = await getRoutine(habitName);
         fetch(`${IP}/newHabitRow?habitName=${encodeURIComponent(habitName)}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 target: target,
-                routine: routine
+                routine: routine,
             }),
-        })
-            .catch(error => console.error('Error:', error));
+        }).catch((error) => console.error("Error:", error));
     }
 
     // GUI til at se og styre værdier
     return (
         <View style={styles.container}>
-
             <Button title="Refresh Data" onPress={getAllData} />
             <Button title="Get target" onPress={() => getTarget(habitName)} />
             <Button title="Get routine" onPress={() => getRoutine(habitName)} />
@@ -120,22 +121,27 @@ export default function App() {
             <Text style={styles.textColor}>Routine = {routine}</Text>
 
             {Object.entries(habitData).map(([key, value]) => (
-                <Text style={styles.textColor} key={key}>{`${key}: ${value} `}</Text>
+                <Text
+                    style={styles.textColor}
+                    key={key}
+                >{`${key}: ${value} `}</Text>
             ))}
 
-            <Button title="New habit row" onPress={() => newHabitRow(habitName)} />
-
+            <Button
+                title="New habit row"
+                onPress={() => newHabitRow(habitName)}
+            />
+            <Button title="New habit row" onPress={createTable} />
         </View>
     );
-};
-
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#222',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#222",
+        alignItems: "center",
+        justifyContent: "center",
     },
 
     textColor: {
