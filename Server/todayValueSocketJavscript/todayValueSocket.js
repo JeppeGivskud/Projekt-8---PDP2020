e4ebb945a5e706a6d2d8bcc3c0c158c5b7751cf1
JeppeGivskud <jgivsk20@student.aau.dk>
2024-05-16 11:17:40 +0200

    should work

--- Server/todayValueSocketJavscript/todayValueSocket.js
+++ Server/todayValueSocketJavscript/todayValueSocket.js
@@ -1,6 +1,25 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const readline = require("readline");
+
+// Create readline interface
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+// Listen for 'k' key press
+process.stdin.on("keypress", (str, key) => {
+    if (key.name === "k") {
+        io.sockets.emit("pressed", "We Pressin Now");
+    }
+});
+// Listen for 'k' key press
+process.stdin.on("keypress", (str, key) => {
+    if (key.name === "j") {
+        var rando = Math.floor(Math.random() * 100) + 1;
+        io.sockets.emit("encoder", rando);
+    }
+});
 
 // Add messages when sockets open and close connections
 io.on("connection", (socket) => {
@@ -10,15 +29,8 @@ io.on("connection", (socket) => {
     });
 });
 
-// Broadcast the current server time as global message, every 1s
-setInterval(() => {
-    var rando = Math.floor(Math.random() * 100) + 1;
-    io.sockets.emit("newTodayValue", { rando });
-    io.sockets.emit("pressed", "We Pressin Now");
-}, 3000);
-
 // Show the index.html by default
-app.get("/", (req, res) => res.sendFile("index.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 // Start the express server
 http.listen(3000, function () {
