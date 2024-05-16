20350e7c882d021d596d191e3ac7ffaf38563e3b
Christoffer <clyhol20@student.aau.dk>
2024-05-16 09:23:55 +0200

    Refractored and commented

--- Server/countSocketPython/Pi_components.py
+++ Server/countSocketPython/Pi_components.py
@@ -1,122 +1,126 @@
-import RPi.GPIO as GPIO  # type: ignore
+import RPi.GPIO as GPIO
 from time import sleep
 import threading
 import socketio
 import eventlet
 
-button_pin = 16
-counter = 0
-enc_A = 35
-enc_B = 33
-button_changed = False
-encoder_changed = False
 
-sio = socketio.Server(async_mode="eventlet", cors_allowed_origins=["http://localhost:8081", "http://hvejsel.dk:8080"])
-app = socketio.WSGIApp(sio)
+class Button:
+    def __init__(self, pin):
+        self.pin = pin
+        self.changed = False
+        self.lock = threading.Lock()
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=self.callback, bouncetime=200)
 
+    def callback(self, channel):
+        sleep(0.05)  # Add debounce delay
+        if GPIO.input(channel) == GPIO.LOW:
+            with self.lock:
+                self.changed = True
+                print("Button pressed")
 
-def send_button():
-    global button_changed, sio
-    while True:
-        if button_changed:
-            sio.emit("pressed", "pressed")
-            print("sent pressed")
-            button_changed = False
-            eventlet.sleep(0.1)
-
-
-def send_encoder():
-    global counter, encoder_changed
-    while True:
-        if encoder_changed:
-            sio.emit("encoder", counter)
-            print("send_encoder: sent ", counter)
-            encoder_changed = False
-            eventlet.sleep(0.1)
+    def send(self, sio):
+        while True:
+            with self.lock:
+                if self.changed:
+                    sio.emit("pressed", "pressed")
+                    print("sent pressed")
+                    self.changed = False
+            eventlet.sleep(0.001)
 
 
-def server_listener():
-    eventlet.wsgi.server(eventlet.listen(("", 3000)), app)
+class Encoder:
+    def __init__(self, pin_A, pin_B):
+        """Initialize the encoder. Attach event detection to pin_A to trigger callback on rotation.
 
+        Args:
+            pin_A (int): Pin number for encoder switch A. Use physical pin numbers.
+            pin_B (int): Pin number for encoder switch B. Use physical pin numbers.
+        """
+        self.pin_A = pin_A
+        self.pin_B = pin_B
+        self.counter = 0
+        self.changed = False
+        self.lock = threading.Lock()
+        GPIO.setup(self.pin_A, GPIO.IN)
+        GPIO.setup(self.pin_B, GPIO.IN)
+        GPIO.add_event_detect(self.pin_A, GPIO.RISING, callback=self.callback, bouncetime=10)
 
-def button_init():
-    GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback, bouncetime=200)
+    def callback(self, channel):
+        # First read
+        switch_A = GPIO.input(self.pin_A)
+        switch_B = GPIO.input(self.pin_B)
 
-    while True:
-        sleep(0.1)  # loop to keep the thread alive
+        # Debounce logic
+        sleep(0.001)
 
+        # Read switches again after debounce
+        switch_A_new = GPIO.input(self.pin_A)
+        switch_B_new = GPIO.input(self.pin_B)
 
-def button_callback(channel):
-    global button_changed
-    sleep(0.05)  # Add debounce delay
-    if GPIO.input(channel) == GPIO.LOW:
-        button_changed = True
-        print("Button pressed")
-
-
-def init_encoder():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(enc_A, GPIO.IN)
-    GPIO.setup(enc_B, GPIO.IN)
-    GPIO.add_event_detect(enc_A, GPIO.RISING, callback=encoder_callback, bouncetime=10)
-
-    while True:
-        sleep(0.1)  # loop to keep the thread alive
-
-
-def encoder_callback(channel):
-    global counter, encoder_changed  # global means that the variable is defined outside of the function
-    switch_A = GPIO.input(enc_A)
-    switch_B = GPIO.input(enc_B)
-
-    # Debounce logic
-    debounce_time = 0.001
-    sleep(debounce_time)
-
-    # Read switches again after debounce
-    switch_A_new = GPIO.input(enc_A)
-    switch_B_new = GPIO.input(enc_B)
-
-    # Check if switches have stabilized
-    if switch_A == switch_A_new and switch_B == switch_B_new:
-        # Determine direction of rotation
-        if switch_A == 1 and switch_B == 0:
-            direction = 1  # Clockwise
-        elif switch_A == 1 and switch_B == 1:
-            direction = -1  # Counterclockwise
-        else:
-            return  # No rotation
-
-        # Update counter based on direction
-        # context manager saying that this can only be accessed by one thread at a time
-        if direction == 1:
-            print("Direction -> ", counter)
-            if counter < 100:
-                counter += 1
+        # Check if switches have stabilized. Stabilized means that the switches have not changed state during the debounce time.
+        if switch_A == switch_A_new and switch_B == switch_B_new:
+            # Determine direction of rotation
+            if switch_A == 1 and switch_B == 0:
+                direction = "clockwise"
+            elif switch_A == 1 and switch_B == 1:
+                direction = "Counterclockwise"
             else:
-                counter = 100
-        elif direction == -1:
-            print("Direction <- ", counter)
-            if counter > 0:
-                counter -= 1
-            else:
-                counter = 0
-        encoder_changed = True
+                return  # No rotation
+
+            # Update counter based on direction
+            if direction == "clockwise":
+                if self.counter < 100:
+                    self.counter += 1
+                else:
+                    self.counter = 100
+                print("Direction -> ", self.counter)
+            elif direction == "Counterclockwise":
+                if self.counter > 0:
+                    self.counter -= 1
+                else:
+                    self.counter = 0
+                print("Direction <- ", self.counter)
+            with self.lock:
+                self.changed = True
+
+    def send(self, sio):
+        """Function for sending the encoder value to the client. This function will run in a separate event thread. It will send the encoder value to the client when the value has changed."""
+        while True:
+            with self.lock:
+                if self.changed:
+                    sio.emit("encoder", self.counter)
+                    print("send_encoder: sent ", self.counter)
+                    self.changed = False
+            eventlet.sleep(0.001)
 
 
-@sio.on("connect")
-def connect(sid, environ):
-    print("connect ", sid)
-    sio.emit("encoder", 0)
+class Server:
+    def __init__(self, button, encoder):
+        """Initialize the server with the button and encoder objects. The server is setup to use the eventlet async_mode. It allows communication from localhost:8081 and hvejsel.dk:8080.
 
-    button_thread = threading.Thread(target=button_init)  # initialize button in a separate thread
-    button_thread.daemon = True  # kill thread when main thread is killed
-    button_thread.start()
+        Args:
+            button (Button object): Pass the button object to the server.
+            encoder (Encoder object): Pass the encoder object to the server.
+        """
+        self.sio = socketio.Server(
+            async_mode="eventlet", cors_allowed_origins=["http://localhost:8081", "http://hvejsel.dk:8080"]
+        )
+        self.app = socketio.WSGIApp(self.sio)
+        self.button = button
+        self.encoder = encoder
+        self.sio.on("connect", self.connect)
 
-    encoder_thread = threading.Thread(target=init_encoder)  # initialize encoder in a separate thread
-    encoder_thread.daemon = True  # kill thread when main thread is killed
-    encoder_thread.start()
+    def connect(self, sid, environ):
+        print("connect ", sid)
+        self.sio.emit("encoder", 0)
+
+        eventlet.spawn(self.button.send, self.sio)
+        eventlet.spawn(self.encoder.send, self.sio)
+
+    def listen(self):
+        eventlet.wsgi.server(eventlet.listen(("", 3000)), self.app)
 
 
 def main():
@@ -124,11 +128,13 @@ def main():
     GPIO.setmode(GPIO.BOARD)
     print("Starting script..\n")
 
+    button = Button(16)
+    encoder = Encoder(35, 33)
+    server = Server(button, encoder)
+
     try:  # run inits and threads
         print("Starting button and encoder\n")
-        eventlet.spawn(send_button)
-        eventlet.spawn(send_encoder)
-        server_listener()  # main thread
+        server.listen()  # main thread
 
     except KeyboardInterrupt:  # stop on keyboard interrupt and cleanup
         print("Stopping\n")
