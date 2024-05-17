d85cbdba796372fc71bef71e7d96c6470b393356
JeppeGivskud <jgivsk20@student.aau.dk>
2024-05-17 15:23:59 +0200

    CONFETTI

--- Physical interface/HabitPuckV1/Components/Overview.js
+++ Physical interface/HabitPuckV1/Components/Overview.js
@@ -2,29 +2,24 @@ import { StyleSheet, View } from "react-native";
 import ProgressBar from "./ProgressBar";
 import OverviewInformation from "./OverviewInformation";
 import WeekHistory from "./WeekHistory";
-//yarn http-server ./dist-withCirclesNew -a 192.168.1.173
-//yarn expo export -p web
+import Confetti from "react-confetti";
 
 export default function Overview({ props }) {
-  const { count, habitName, target, width, historyCounts, habitColor } = props;
-
-  return (
-    <View style={[styles.container]}>
-      <ProgressBar count={count} target={target} habitColor={habitColor} />
-      <OverviewInformation
-        count={count}
-        target={target}
-        habitName={habitName}
-        width={width}
-      />
-      <WeekHistory historyCounts={historyCounts} habitColor={habitColor} />
-    </View>
-  );
+    const { count, habitName, target, width, historyCounts, habitColor } = props;
+    console.log(props.count);
+    return (
+        <View style={[styles.container]}>
+            {!!(count == target) && <Confetti width={250} height={250} />}
+            <ProgressBar count={count} target={target} habitColor={habitColor} />
+            <OverviewInformation count={count} target={target} habitName={habitName} width={width} />
+            <WeekHistory historyCounts={historyCounts} habitColor={habitColor} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
 });
