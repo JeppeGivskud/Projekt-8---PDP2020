c35ddc40b25910c593bff85d9b2afbb0c34ec29f
JeppeGivskud <jgivsk20@student.aau.dk>
2024-05-28 13:01:51 +0200

    asdf

--- Physical interface/PEPV2/App.js
+++ Physical interface/PEPV2/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [habitData, setHabitData] = useState({
+        habitName: "",
+        count: { 0: 1, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 },
+        target: 100,
+        streak: { count: 0, omissions: 0 },
+        routine: "DefaultRutine",
+        effort: 0,
+    });
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
