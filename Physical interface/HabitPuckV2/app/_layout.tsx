642e52331ce36baef03ea2f65e337ec9f9e850e4
JeppeGivskud <jgivsk20@student.aau.dk>
2024-05-28 09:18:31 +0200

    Change to data

--- Physical interface/HabitPuckV2/app/_layout.tsx
+++ Physical interface/HabitPuckV2/app/_layout.tsx
@@ -1,10 +1,18 @@
 import { Stack } from "expo-router";
+import { useState } from "react";
 
 export default function RootLayout() {
+    const [habitData, setHabitData] = useState({
+        habitName: "Undefined",
+        count: 0,
+        target: 100,
+        habitColor: "black",
+        streak: { count: 0, omissions: 0 },
+    });
     return (
         <Stack>
             <Stack.Screen name="index" />
-            <Stack.Screen name="./DefaultApp/Overview" />
+            <Stack.Screen name="./DefaultApp/Overview" {...{ habitData }} />
             <Stack.Screen name="./DefaultApp/Effort" />
             <Stack.Screen
                 name="./DefaultApp/Done
